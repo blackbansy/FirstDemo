f81a91acdcfd91789bd11bd04ff2ee6bb030c898
chenzc <bansy17@gmail.com>
2018-06-26 21:30:16 +0800

    【类型】需求开发 【内容】添加两个按钮 【影响】无 类型：1.修复BUG 2.需求开发 3.代码优化 4.需求调整

---
+++ FirstDemo/FirstDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstDemo
+//
+//  Created by Tomas Tech on 26/06/2018.
+//  Copyright © 2018 Tomas Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstDemo/FirstDemo/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  FirstDemo
+//
+//  Created by Tomas Tech on 26/06/2018.
+//  Copyright © 2018 Tomas Tech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *leftBtn;
+@property (weak, nonatomic) IBOutlet UIButton *rightBtn;
+
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)leftBtnClick:(id)sender {
+}
+
+
+- (IBAction)rightBtnClick:(id)sender {
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
